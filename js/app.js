4598365e5292f63048ed09cd4fd1f6df9f2031a1
TyreseCardinal <tyrese.k.cardinal@shaw.ca>
2024-03-27 20:39:20 -0600

    Knowledge Check #3

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-let user_points = 74;
+let user_points = 75
 
 if (user_points >= 75) {
   console.log("Congratulations!");
+} else if (user_points >= 50 ) {
+  console.log("Keep going, almost at 75");
 } else { 
   (user_points < 74) 
   console.log("Points too low, try again");
